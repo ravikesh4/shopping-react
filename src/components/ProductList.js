bf9a3a16d15a182c925301d4f650dd6f05efad00
ravikesh yadav <ravikesh@netzary.com>
2021-03-24 00:15:40 +0530

    cart done

---
+++ src/components/ProductList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useFilterContext } from '../context/filter_context'
+import GridView from './GridView'
+import ListView from './ListView'
+
+const ProductList = () => {
+
+  const {filtered_products: products, grid_view} = useFilterContext();
+  // console.log(products);
+
+  if(products.length < 1) {
+    return <h5 style={{textTransform:'none'}}>
+      Sorry, No Product...
+    </h5>
+  }
+
+  if(grid_view === false) {
+    return <ListView products={products} />
+  }
+
+  return <GridView products={products}>
+
+  </GridView>
+}
+
+export default ProductList

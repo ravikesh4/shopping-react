f735c5787164d412eb8594a133d99318c9171364
ravikesh yadav <ravikesh@netzary.com>
2021-03-25 11:05:34 +0530

    Updates

---
+++ precache-manifest.e68274a577b4e9d858bae69f7d378058.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b723efc08c6960e3a919c9238c34266b",
+    "url": "/shopping-react/index.html"
+  },
+  {
+    "revision": "e9cefb8a730207f7fd93",
+    "url": "/shopping-react/static/css/main.1c23fea6.chunk.css"
+  },
+  {
+    "revision": "b2c118e8e05109f214ff",
+    "url": "/shopping-react/static/js/2.1ce6bef9.chunk.js"
+  },
+  {
+    "revision": "cb49b755353ff989abbd5eca41920e9f",
+    "url": "/shopping-react/static/js/2.1ce6bef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9cefb8a730207f7fd93",
+    "url": "/shopping-react/static/js/main.c608887e.chunk.js"
+  },
+  {
+    "revision": "7525624d3e63713374aa",
+    "url": "/shopping-react/static/js/runtime-main.77169eb3.js"
+  },
+  {
+    "revision": "64f4f61027c492c72df44225fcdea738",
+    "url": "/shopping-react/static/media/hero-bcg-2.64f4f610.jpeg"
+  },
+  {
+    "revision": "1f293b1bf1a4886166cc4befd5cfd839",
+    "url": "/shopping-react/static/media/hero-bcg.1f293b1b.jpeg"
+  },
+  {
+    "revision": "b2b8d556608782fcee0183d04ab82009",
+    "url": "/shopping-react/static/media/logo.b2b8d556.svg"
+  }
+]);
